90c5ec9a9f862e2465d092d9e22ab62b04d35c39
WeaverKyle <weaver_kyleJ@yahoo.com>
2016-05-14 21:36:50 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Very similar to the numeric average example, yet now we are looking at a matrix. 
+## The form of the function is identical to the example, we only replace "m" with "inv." 
+## makeCacheMatrix creates a special vector which sets and gets the vector, and sets and gets the inverse
+## of said vector. In other words, it stores our values so the program will not have to recalculate with
+## each itiration. 
+makeCacheMatrix <- function(x = matrix()) { 
+  inv=NULL
+  set=function(y) {
+    x<<-y
+    inf<<-NULL
+    
+  }
+get=function ()x
+setinv= Function (inverse)inv<<-inverse
+getinv=function ()inv
+list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes an inverse of the special matrix we durived above (makeCacheMatrix)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        inv=x$getinv()
+        if(!is.null(inv)){
+        return(inv)
+        }
+    data=x$get()
+    inv=solve(data,...)
+    x$setinv(inv)
+    return(inv)
+        }
